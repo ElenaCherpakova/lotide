6de425d811d18ec0bacf10cbb625aaa07e51aec7
ElenaCherpakova <elena.cherpakova@gmail.com>
2021-11-22 23:01:10 +0000

    head

--- head.js
+++ head.js
@@ -7,6 +7,10 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const head = function(array) {
+  return array[0];
+};
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]),);
