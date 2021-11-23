8ff55a5342ce379df10b6a2f51e8dcac8c8c5649
ElenaCherpakova <elena.cherpakova@gmail.com>
2021-11-23 02:34:27 +0000

    refactored pig-latin

--- pig-latin.js
+++ pig-latin.js
@@ -1,9 +1,8 @@
 const args = process.argv.slice(2);
 const pigLatin = function(string) {
-  let sliced = string.slice(1);
-  return sliced + string[0] + "ay";
+  return string.slice(1) + string[0] + "ay";
 };
 
-for (let i = 0; i < args.length; i++) {
-  console.log(pigLatin(`${args[i]}`));
+for (el of args) {
+  console.log(pigLatin(`${el}`));
 }
