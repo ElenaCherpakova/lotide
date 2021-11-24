1f7cc0e98932cc2245904e21bdb54048c3141c22
ElenaCherpakova <elena.cherpakova@gmail.com>
2021-11-24 02:17:48 +0000

    refactoring

--- eqArrays.js
+++ eqArrays.js
@@ -1,8 +1,8 @@
 const eqArrays = function(arr1, arr2) {
-  let i = arr1.length;
-  if (i !== arr2.length) return false;
-  while (i--) {
-    if (arr1[i] !== arr2[i]) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
   }
   return true;
 };
