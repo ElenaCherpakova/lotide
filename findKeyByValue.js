61caa229c6911c5b9788ecabc2316b87ab3818ac
ElenaCherpakova <elena.cherpakova@gmail.com>
2021-11-25 04:55:09 +0000

    debugged

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,10 +1,25 @@
-const assertEqual = function(actual, expected) {
-  return (actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`));
+const eqArrays = function(arr1, arr2) {
+  if (arr1 !== arr2) {
+    return false;
+  } for (var i = 0; i < arr1; i++){
+    if (arr1[i] !== arr2[i]){
+      return false;
+    }
+  }
+  return true;
 };
 
+const assertEqual = function (arr1, arr2) {
+  const result = eqArrays (arr1, arr2);
+    if (result) {
+      return (`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+    } else {
+      return (`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+}
+}
 
 const findKeyByValue = function(bestShow, value) {
-  const keyGenre = Object.keys(bestShow); // Object.keys function to help you easily search through all the object keys.
+  const keyGenre = Object.keys(bestShow); 
   for (let showName of keyGenre) {
     if (bestShow[showName] === value) return showName;
   }
@@ -18,4 +33,4 @@ const bestTVShowsByGenre = {
 };
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual((findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
