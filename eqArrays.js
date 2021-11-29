6d17e5b6d177e716a449418d2cd1453dee29572b
ElenaCherpakova <elena.cherpakova@gmail.com>
2021-11-29 19:31:10 +0000

    refactoring

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,13 @@
 const eqArrays = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+    if (arr1[i] === arr2[i]) {
+      return true;
     }
   }
-  return true;
+  return false;
 };
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+
+
+module.exports = eqArrays;
+
 
